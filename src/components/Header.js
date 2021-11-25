f26eeba26a66f111d5900f933175280906e1710c
Nick Shirokov <nick_4312@mail.ru>
2021-11-25 20:53:00 +0300

    feat: Добавлена ссылка навигации в шапке

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,35 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
-function Header(props) {
+function Header({loggedIn}) {
+
+  const { pathname } = useLocation();
+  const [ linkProps, setLinkProps ] = useState({href: '', text: ''})
+
+  useEffect(() => {
+    if (pathname === '/sign-in') {
+      setLinkProps({
+        href: '/sign-up',
+        text: 'Регистрация'
+      })
+    } else if (loggedIn) {
+      setLinkProps({
+        href: '/sign-in',
+        text: 'Выйти'
+      })
+    } else {
+      setLinkProps({
+        href: '/sign-in',
+        text: 'Войти'
+      })
+    }
+  }, [pathname, loggedIn])
+
   return (
     <header className="header">
       <Link to="/" className="header__logo" aria-label="Перейти на главную"></Link>
+      <div className="space"></div>
+      <Link to={linkProps.href} className="header__link button">{linkProps.text}</Link>
     </header>
   );
 }
